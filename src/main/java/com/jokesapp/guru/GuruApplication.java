98c8b658ff0e7450f857cd4b34307ab5e74c6650
muhammadsiyadofficial@gmail.com <muhammadsiyadofficial@gmail.com>
2019-01-16 10:04:09 +0530

    Initial commit

---
+++ src/main/java/com/jokesapp/guru/GuruApplication.java
@@ -0,0 +1,14 @@
+package com.jokesapp.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruApplication.class, args);
+    }
+
+}
+
